6f4e7840c4a5e6469aaea5d5ca0a2bbba278ea78
Leon <angry_yang@163.com>
2018-07-18 16:25:11 +0800

    测试rmi传递对象

---
+++ src/main/java/org/yangxin/HashService.java
@@ -0,0 +1,14 @@
+package org.yangxin;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+
+/**
+ * @author leon on 2018/7/18.
+ * @version 1.0
+ */
+public interface HashService extends Remote {
+
+    HashMap<Integer, String> getHash(String name) throws RemoteException;
+}
